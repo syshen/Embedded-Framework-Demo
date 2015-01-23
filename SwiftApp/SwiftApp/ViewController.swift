5f8eeaf20e7aa7111ae6a84a30be831ea9afc402
syshen <sysheen@gmail.com>
2015-01-23 15:48:06 +0800

    Make sure every arch swiftmodule are in place

---
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by syshen on 1/23/15.
+//  Copyright (c) 2015 Intelligent Gadget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
